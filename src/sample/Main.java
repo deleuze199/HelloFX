e05ca67525763e8d5f8af1b3fd78a0a8b2afc2bc
bldeleuze1232 <bldeleuze1232@eagle.fgcu.edu>
2019-08-21 19:17:34 -0400

    Activity 1

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,7 +16,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World?");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Button btn = new Button();
+        btn.setText("Say, 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("hello World!");
+            }
+        });
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 255));
         primaryStage.show();
     }
 
